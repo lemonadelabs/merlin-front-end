17fec719eb1e469317f1a7052ead09327e469ba6
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-03-24 12:11:37 +1300

    made the entity node colour changeable by adding variables in css

--- app/components/entity-node/component.js
+++ app/components/entity-node/component.js
@@ -2,13 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames:['entity-node'],
-  classNameBindings:['id'],
+  classNameBindings:['id',"entity-type"],
   id: undefined,
+  "entity-type":undefined,
 
   didInsertElement: function () {
     this.nodes.push(this)
     this.nodes.arrayContentDidChange(this.nodes.length, 0, 1)
     this.set('id',this.entity.id);
+    this.set('entity-type',`entity-${this.entity.tags[0]}`);
   }
 
 });
