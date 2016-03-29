706e5e740210362d49cae43024d21588c484d89a
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-03-29 13:49:50 +1300

    component now moves, kinda

--- app/components/draggy-stretchy/component.js
+++ app/components/draggy-stretchy/component.js
@@ -1,10 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  active:false,
+  attributeBindings: ['style'],
+  style:Ember.computed('x', function(){
+    var x = this.get('x');
+    return(`transform:translate(${x}px)`);
+  }),
+  x:0,
   didInsertElement(){
-    console.log(Ember.$('document').data('events'));
-    Ember.$('document').mousemove(function(e){
-      console.log(e);
-    });
+    document.onmousemove = document.onmousemove || this.updateInputPosition;
+  },
+  willDestroy(){
+    document.onmousemove = null;
+  },
+  mouseDown(e){
+    this.set('active',true);
+    Ember.run.next(this,this.updateMyPosition,{'e':e});
+
+  },
+  mouseUp(){
+    console.log('mouseMouseUp');
+    this.set('active',false);
+    this.get('onMouseUp');
+  },
+  updateMyPosition:function(args){
+    var e = args.e;
+    //fallback for old firefox that doesn't support offsetX
+    var offset = e.offsetX || (document.inputX - this.get('x')) - 8;
+    this.set('x', document.inputX - offset);
+    if(this.get('active')){
+      // Ember.run.next(this,this.updateMyPosition,{'e':e});
+    }
+  },
+  updateInputPosition: function(e){
+    // console.log(e);
+    document.inputX = e.clientX;
+    document.inputY = e.clientY;
   }
+
 });
