cf50f7d1023996b799f395135b886c590d83f6c8
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-04-11 14:25:42 +1200

    updated url to reflect backend changes

--- app/wills-house/route.js
+++ app/wills-house/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return Ember.$.getJSON('simulations/1')
+    return Ember.$.getJSON('api/simulations/1')
     // return mockData()
   }
 });
