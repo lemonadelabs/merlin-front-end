64a88293fd930b355a0d30f61e0839b2f57e88b2
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-04-15 10:12:24 +1200

    fixed error with nested arrays

--- app/components/lemonade-chart/chartParameters.js
+++ app/components/lemonade-chart/chartParameters.js
@@ -1,12 +1,11 @@
 export default class ChartParameters {
-  constructor(DataSets, Labels, xAxes, yAxes){
-    this.options = {
-      scales:{
-        xAxes:[xAxes],
-        yAxes:[yAxes]
-      }
-    }
-    this.data.labels = Labels;
-    this.data.datasets.pushObjects(DataSets);
+  constructor(DataSets, Labels, xAxes, yAxes) {
+    this.options = {}
+    this.options.scales = {}
+    this.options.scales.xAxes = xAxes
+    this.options.scales.yAxes = yAxes
+    this.data = {}
+    this.data.labels = Labels
+    this.data.datasets = DataSets
   }
 }
