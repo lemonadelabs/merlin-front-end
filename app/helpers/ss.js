c2930f442d16230b010ff2596ed88bcef83f9bf2
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-04-21 11:27:05 +1200

    debugging for style binding warning

--- app/helpers/ss.js
+++ app/helpers/ss.js
@@ -4,7 +4,7 @@ import { htmlSafe } from 'ember-string';
 export default Helper.extend({
   compute(params) {
     let string = params[0];
-
+    console.log(htmlSafe(string));
     return htmlSafe(string);
   }
 });
