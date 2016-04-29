406059175b0c9f062d334f9241cafa5a02383d19
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-04-29 15:27:47 +1200

    basic functionality and styling

--- app/components/time-input-node-editor/component.js
+++ app/components/time-input-node-editor/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['time-input-container']
+  classNames: ['time-input-container'],
+  months: ['Jul','Aug','Sep','Oct','Nov','Dec','Jan','Feb','Mar','Apr','May','Jun'],
+  actions:{
+    changeMonth: function(month){
+      let months = this.get('months');
+      let index = _.findIndex(months, function(o) {/*jshint eqeqeq: false*/ return o == month; });
+      this.set('month', index+1);
+    }
+  }
 });
