5dc70da69ddaddf204c7f4fd210796d049e330b0
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-05-02 13:42:43 +1200

    loading projects

--- app/components/timeline-track/component.js
+++ app/components/timeline-track/component.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['timeline-track']
+  classNames: ['timeline-track'],
+  timelineObjects: undefined,
+  init(){
+    this._super();
+    this.set('timelineObjects', []);
+    console.log(this.model);
+    let self = this;
+    _.forEach(this.model,function(value, key){
+        if(typeof value === 'object'){
+          value.name = key
+          self.timelineObjects.push(value)
+        }
+    })
+  }
 });
