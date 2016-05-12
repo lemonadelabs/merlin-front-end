72ce03decd14290917216562314a0b1207ae8914
will-sklenars <will.sklenars@gmail.com>
2016-05-12 12:08:53 +1200

    wizard skeleton functioning

--- app/components/new-project-3/component.js
+++ app/components/new-project-3/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    next () {
+      this.set('submitted', true);
+      // do things regarding data, like validation
+      this.sendAction('next');
+    },
+
+    cancel () {
+      this.sendAction('cancel');
+    }
+  }
 });
--- app/components/new-project-wizard/component.js
+++ app/components/new-project-wizard/component.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  steps: ['new-project-1', 'new-project-2', 'new-project-3'],
+  currentStep: undefined,
+
+  init: function () {
+    this._super()
+    this.set('currentStep', this.get('steps')[0])
+  },
+
+  actions: {
+    next () {
+      let steps = this.get('steps'),
+      index = steps.indexOf(this.get('currentStep'));
+
+      this.set('currentStep', steps[index + 1]);
+    },
+
+    back () {
+      let steps = this.get('steps'),
+      index = steps.indexOf(this.get('currentStep'));
+
+      this.set('currentStep', steps.get(index - 1));
+    },
+
+    cancel () {
+      // this.transitionToRoute('somewhere-else');
+    },
+
+    finish () {
+      // this.transitionToRoute('wizard-finished');
+    }
+  },
+
 });
+
