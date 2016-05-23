1a379783931a4857df1089859b7310d7eb65c57e
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-05-23 16:51:18 +1200

    rudimentary integration of the new project form with the plan view

--- app/components/plan-component/component.js
+++ app/components/plan-component/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showNewProjectWiz: false,
+  actions:{
+    newProject: function(){
+      this.set('showNewProjectWiz', true);
+    },
+    hideNewProject: function(){
+      this.set('showNewProjectWiz', false);
+    }
+  }
 });
