f53c6fca30c26a5b393750a17d67bf17e904b73c
will-sklenars <will.sklenars@gmail.com>
2016-05-25 14:47:48 +1200

    router is converting timel

--- app/plan/route.js
+++ app/plan/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import * as convertTime from '../common/convert-time-es6'
 import mockData from './mock-data'
 
 export default Ember.Route.extend({
@@ -6,12 +7,31 @@ export default Ember.Route.extend({
   model: function (params) {
     return Ember.RSVP.hash({
       simulation: Ember.$.getJSON(`api/simulations/${params.simulation_id}`),
-      plan: mockData()
+      plan: mockData(),
+      projects: Ember.$.getJSON('api/projects')
     });
   },
 
   setupController: function (controller, models) {
+    this.convertTimeInModels(models)
     controller.setProperties(models);
-  }
+    var projects = this.get('projects')
+  },
+
+  convertTimeInModels: function (models) {
+    var projects = models.projects
+    _.forEach(projects, this.convertTimePhases.bind(this))
+  },
+
+  convertTimePhases: function (project) {
+    _.forEach(project.phases, this.convertTimesInObject)
+  },
+
+  convertTimesInObject: function (object) {
+    _.forEach(object, function (value, key) {
+      object[key] = (key.indexOf('date') > -1) ? convertTime.toQuater(value) : value
+    })
+  },
+
 
 });
