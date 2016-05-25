e5b8e6eb5c1dd579e9193f7a0c3e0fc1a85610fc
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-05-26 11:37:57 +1200

    haircut route and component in place and in the main nav

---
+++ app/haircut/route.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function (params) {
+    return Ember.RSVP.hash({
+      simulation: Ember.$.getJSON(`api/simulations/${params.simulation_id}`),
+      'simulation-run': Ember.$.getJSON(`api/simulation-run/${params.simulation_id}`)
+    });
+  },
+});
