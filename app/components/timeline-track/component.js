5b4569f3783c366dcdcf492f0c96147d90a4a9b5
will-sklenars <will.sklenars@gmail.com>
2016-05-25 14:48:08 +1200

    timeline track is displaying the blocks

--- app/components/timeline-track/component.js
+++ app/components/timeline-track/component.js
@@ -4,21 +4,21 @@ export default Ember.Component.extend({
   classNames: ['timeline-track'],
   // timelineObjects: undefined,
   // init(){
-    // this._super();
-    // let self = this;
-    // if (!this.get('timelineObjects')) {
-    //   this.set('timelineObjects', []);
+  //   this._super();
+  //   let self = this;
+  //   if (!this.get('timelineObjects')) {
+  //     this.set('timelineObjects', []);
 
-    //   _.forEach( this.model, function (value, key){
-    //     if(typeof value === 'object'){
-    //       console.log('key', key)
-    //       console.log('value', value)
-    //       value.name = key
-    //       self.timelineObjects.push(value)
-    //     }
-    //   })
+  //     _.forEach( this.model, function (value, key){
+  //       if(typeof value === 'object'){
+  //         console.log('key', key)
+  //         console.log('value', value)
+  //         value.name = key
+  //         self.timelineObjects.push(value)
+  //       }
+  //     })
 
-    // }
+  //   }
   // },
 
 });
