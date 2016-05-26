314a6025f1c13cc3a008f8d015f0570e4b23bc04
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-05-26 12:12:53 +1200

    haircut component gets the services and budgets for that component

--- app/components/haircut-component/component.js
+++ app/components/haircut-component/component.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import * as simTraverse from '../../common/simulation-traversal';
 
 export default Ember.Component.extend({
+  services:undefined,
+
+  didInsertElement(){
+    Ember.run.next(this,this.setup)
+  },
+  setup(){
+    let self = this,
+        simulation = this.get('simulation'),
+        serviceModels = simTraverse.getServiceModelsFromSimulation({simulation : simulation})
+    this.set('services', serviceModels)
+
+    console.log(serviceModels);
+    _.forEach(serviceModels,function(serviceModel){
+      console.log(serviceModel);
+      let budgetsForService = simTraverse.getChildEntitiesByAttribute({'simulation':simulation,'serviceModel':serviceModel,'attribute':'budget'})
+      console.log(budgetsForService);
+    })
+  }
 });
