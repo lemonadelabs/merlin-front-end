91a0101539aea42e9709abbb8a49ae77c09d422c
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-05-26 13:49:36 +1200

    added services pool

--- app/components/haircut-component/component.js
+++ app/components/haircut-component/component.js
@@ -2,22 +2,31 @@ import Ember from 'ember';
 import * as simTraverse from '../../common/simulation-traversal';
 
 export default Ember.Component.extend({
-  services:undefined,
-
+  servicesPool:[],
   didInsertElement(){
     Ember.run.next(this,this.setup)
   },
   setup(){
     let self = this,
         simulation = this.get('simulation'),
-        serviceModels = simTraverse.getServiceModelsFromSimulation({simulation : simulation})
-    this.set('services', serviceModels)
+        serviceModels = simTraverse.getServiceModelsFromSimulation({simulation : simulation}),
+        servicesPool = this.get('servicesPool')
+    if(servicesPool.length > 0){
+      this.set('servicesPool', [])
+    }
 
     console.log(serviceModels);
+
     _.forEach(serviceModels,function(serviceModel){
       console.log(serviceModel);
-      let budgetsForService = simTraverse.getChildEntitiesByAttribute({'simulation':simulation,'serviceModel':serviceModel,'attribute':'budget'})
+      let budgetsForService = simTraverse.getChildEntitiesByAttribute({'simulation':simulation,'serviceModel':serviceModel,'attribute':'budget'}),
+          servicesPoolObject = {};
+
+      servicesPoolObject['service'] = serviceModel;
+      servicesPoolObject['budget'] = budgetsForService;
+      servicesPool.push(servicesPoolObject)
       console.log(budgetsForService);
     })
+  console.log(servicesPool);
   }
 });
