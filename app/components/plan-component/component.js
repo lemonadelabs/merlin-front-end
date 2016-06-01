f947f6a1a16634b5a70fc2fa3d666ad7f6c31653
will-sklenars <will.sklenars@gmail.com>
2016-06-01 15:26:08 +1200

    timeline objects are rendering in the right place

--- app/components/plan-component/component.js
+++ app/components/plan-component/component.js
@@ -1,24 +1,36 @@
 import Ember from 'ember';
+import * as convertTime from '../../common/convert-time-es6'
 
 export default Ember.Component.extend({
   showNewProjectWiz: false,
-
   reloadProjects: function () {
     var self = this
 
     Ember.$.getJSON('api/projects').then(function (returned) {
-      var projects = self.get('projects')
-      var amountProjects = projects.length
-      var amountReturned = returned.length
-      var diff = amountReturned - amountProjects
-      var newProjects = returned.splice(amountProjects, diff)
-      console.log('newProjects', newProjects)
-      _.forEach(newProjects, function (newProject) {
-        projects.push(newProject)
-        self.projects.arrayContentDidChange(self.projects.length, 0, 1)
-      })
+      var projects = returned
+      // var projects = self.get('projects')
+      // var amountProjects = projects.length
+      // var amountReturned = returned.length
+      // var diff = amountReturned - amountProjects
+      // var newProjects = returned.splice(amountProjects, diff)
+
+      self.convertTimeInProjects(projects)
+
+      // console.log('newProjects', newProjects)
+      // _.forEach(newProjects, function (newProject) {
+      //   projects.push(newProject)
+      //   self.projects.arrayContentDidChange(self.projects.length, 0, 1)
+      // })
       self.set('projects', projects)
     })
+  },
+
+  convertTimeInProjects: function (projects) {
+    _.forEach(projects, this.convertTimePhases.bind(this))
+  },
+
+  convertTimePhases: function (project) {
+    _.forEach(project.phases, convertTime.convertTimesInObject)
   },
 
   actions:{
