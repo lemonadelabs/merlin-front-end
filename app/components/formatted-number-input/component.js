48c8968fb2102ccbf421b6598e08944be4198113
will-sklenars <will.sklenars@gmail.com>
2016-06-07 17:38:08 +1200

    added formatted-number-input component

---
+++ app/components/formatted-number-input/component.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  formattedValue: undefined,
+  originalValue: undefined,
+
+  didInsertElement: function () {
+    this.initValues()
+  },
+
+  initValues: function () {
+    this.set('formattedValue', this.get('value'))
+    this.set('originalValue', this.get('value'))
+  },
+
+  formatValue: function () {
+    var numberValue = Number( this.get( 'formattedValue' ).toString().replace( /,/g, '' ) )
+    if( !isNaN( numberValue ) ) {
+      this.set( 'formattedValue', numberValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
+      this.set( 'value', numberValue );
+    }
+    else{
+      console.warn('number is NaN')
+      this.initValues()
+    }
+  }.observes('formattedValue')
+});
