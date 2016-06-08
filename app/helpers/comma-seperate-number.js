18a347d82c796a76e716e4b4e1b12ee85afbeb95
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-06-08 16:56:04 +1200

    removed log from comma-seperate-number

--- app/helpers/comma-seperate-number.js
+++ app/helpers/comma-seperate-number.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import commaSeperate from '../common/commaSeperateNumber';
 export function commaSeperateNumber(params, hash) {
-  console.log(hash);
   return commaSeperate(hash.number);
 }
 
