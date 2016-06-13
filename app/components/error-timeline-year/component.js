d2d5ba64b5820b8ac7be951200c11010a977924d
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-06-13 16:17:50 +1200

    added mouse over to error timeline

--- app/components/error-timeline-year/component.js
+++ app/components/error-timeline-year/component.js
@@ -3,9 +3,39 @@ import commaSeperate from '../../common/commaSeperateNumber';
 import toTwoDP from '../../common/toTwoDP';
 
 export default Ember.Component.extend({
+  classNames:['error-timeline-year'],
+  popper:undefined,
   mouseEnter(){
     let errorMessages = this.get('year.errorsMessages')
-    this.constructErrorMessage(errorMessages)
+    var formattedMessage = this.constructErrorMessage(errorMessages)
+    this.addPopper(formattedMessage)
+
+  },
+  addPopper(content){
+    if(!content){
+      return
+    }
+
+    var reference = this.get('element');
+    var popper = new Popper(
+        reference,
+        {
+            content: content,
+            contentType: 'html'
+        },
+        {
+             placement: 'top',
+             removeOnDestroy: true,
+             offset: 6
+        }
+    );
+    this.set('popper',popper)
+  },
+  mouseLeave(){
+    var popper = this.get('popper')
+    if(popper){
+      popper.destroy()
+    }
   },
   constructErrorMessage(errorMessages){
     let combinedMessage = ""
@@ -17,9 +47,10 @@ export default Ember.Component.extend({
                             'LS_work_hr':'"Line Staff Work Hours"'
                           })
       let messagWithformatedNumbers = self.findAndFormatNumbers(messageFindReplace)
-      combinedMessage += messagWithformatedNumbers+"\n"
+      combinedMessage += "<p>"+messagWithformatedNumbers+"</p>"
     })
     console.log(combinedMessage);
+    return combinedMessage
   },
   findAndReplaceTemplate(message,lookup){
     var firstSplit = message.split("{{")
