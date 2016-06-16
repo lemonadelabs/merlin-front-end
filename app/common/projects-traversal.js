7eb72c60f5e172123837a15a0bb2046a081763f0
will-sklenars <will.sklenars@gmail.com>
2016-06-16 17:52:42 +1200

    plan view now updates

--- app/common/projects-traversal.js
+++ app/common/projects-traversal.js
@@ -2,6 +2,7 @@ export function getScenarioIds (projects) {
   var scenarios = []
   _.forEach( projects, function ( project ) {
     _.forEach(project.phases, function ( phase ) {
+      console.log(phase)
       scenarios.push(phase.scenario)
     })
   })
