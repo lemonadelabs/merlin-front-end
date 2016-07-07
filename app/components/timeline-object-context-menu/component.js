2a2ef7da6ad2085ae6a6b1b462c726fcbb968ef0
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-07-07 17:04:38 +1200

    add class

--- app/components/timeline-object-context-menu/component.js
+++ app/components/timeline-object-context-menu/component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames:['timeline-object-context-menu'],
+  
   actions:{
     passActionName:function(actionName){
       console.log(actionName);
