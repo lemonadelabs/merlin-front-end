70b3bc24330d54b6f240c6142ccff95ea8355652
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-07-08 11:57:26 +1200

    removing logging

--- app/components/timeline-object-context-menu/component.js
+++ app/components/timeline-object-context-menu/component.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames:['timeline-object-context-menu'],
-  
+
   actions:{
     passActionName:function(actionName){
-      console.log(actionName);
       this.sendAction('handleContextMenuAction', actionName)
     }
   }
