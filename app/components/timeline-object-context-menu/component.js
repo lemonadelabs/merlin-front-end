e2c2648541a4729cf036ad81477c2d88045aaae3
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-07-13 11:53:18 +1200

    context menu fading in

--- app/components/timeline-object-context-menu/component.js
+++ app/components/timeline-object-context-menu/component.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames:['timeline-object-context-menu'],
-
+  showMenu:false,
   actions:{
     passActionName:function(actionName){
       this.sendAction('handleContextMenuAction', actionName)
     }
-  }
+  },
+  observeShowContextMenu:function(){
+    console.log(this.get('showContextMenu'));
+    if(this.get('showContextMenu')){
+      this.set('showMenu',true);
+    }
+  }.observes('showContextMenu')
 });
