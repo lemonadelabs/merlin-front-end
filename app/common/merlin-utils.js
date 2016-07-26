cd097b1cdddea1511b94c90eaa3b07ab5b9d5b85
will-sklenars <will.sklenars@gmail.com>
2016-07-26 12:02:53 +1200

    documented merlin-utils

--- app/common/merlin-utils.js
+++ app/common/merlin-utils.js
@@ -1,3 +1,18 @@
+/**
+* useful misc merlin-specific functions.
+* @module merlin-utils
+*/
+
+
+/**
+* takes a set of telemetry information in monththly devisions, and returns the information in quarterly devisions
+*
+* @method convertDatasetToQuarters
+* @param {Object} opts
+*   @param {Array} opts.dataset
+* @return {Array} dataset
+*/
+
 export function convertDatasetToQuarters(opts) {
   var dataset = opts.dataset
   var returnData = []
@@ -9,11 +24,18 @@ export function convertDatasetToQuarters(opts) {
     var denominator = (i < dataset.length - remainder) ? 3 : remainder ;
     returnData[index] += dataset[i] / denominator
   }
-
   return returnData
-
 }
 
+/**
+* constructs a url to the simulation-run endpoint for a given simulation and set of scenario ids
+*
+* @simulationRunUrl
+* @param {Object} opts
+*   @param {Number} opts.simulationId
+*   @param {Array} opts.scenarioIds
+* @return {String} url
+*/
 export function simulationRunUrl(opts) {
   var scenarioIds = opts.scenarioIds
   var simulationId = opts.simulationId
@@ -26,6 +48,20 @@ export function simulationRunUrl(opts) {
   // 'api/simulation-run/' + id + '/?steps=' + timeframe + '&s0=' + baselineId (example url)
 }
 
+/**
+* creates a `modify process property` action
+*
+* @method createModifyProcessAction
+* @param {Object} opts
+*   @param {Number} opts.entityId
+*   @param {Boolean} opts.revert
+*   @param {Object} opts.newProcessProperty
+*   @param {Number} opts.processPropertyId
+*   @param {Numeber or String} opts.newValue
+*   @param {Numeber or String} opts.oldValue
+
+* @return {Object} json packet action
+*/
 export function createModifyProcessAction(opts) {
   var entityId = opts.entityId
   var props, value, processPropertyId
@@ -70,12 +106,28 @@ export function createModifyProcessAction(opts) {
   }
 }
 
+/**
+* takes an action, and returns an action with the inverse effect
+*
+* @method createInvertedAction
+* @param {Object} opts
+*   @param {Object} opts.action
+* @return {Object} inverted action
+*/
+
 export function createInvertedAction (opts) {
   var action = _.cloneDeep(opts.action)
   action.operand_2.params[1] = (action.operand_2.params[1]) * ( -1 )
   return action
 }
 
+/**
+* @method newEventObject
+* @param {Object} opts
+*   @param {Number} opts.scenarioId
+*   @param {Number} opts.time
+* @return {Object} event object
+*/
 export function newEventObject(opts) {
   var scenarioId = opts.scenarioId,
       time = opts.time
